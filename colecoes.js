7ebee9e61612ad6244e6d54caf36412a00923934
alinevitoriano <alinevitorianoa@gmail.com>
2025-03-22 23:50:03 -0300

    Aprendendo conceitos de coleções e variáveis

---
+++ colecoes.js
@@ -0,0 +1,32 @@
+// Arrays
+
+// let numeros = [10, 20 ,30 ,40, 50]
+
+// console.log(numeros[0])
+// console.log(numeros[1])
+// console.log(numeros[2])
+// console.log(numeros[3])
+// console.log(numeros[4])
+
+let frutas = ["Cajá", "Umbu", "Maça", "Tangerina"];
+console.log(frutas);
+
+frutas.push("Uva"); // Adiciona ao final da lista ["Cajá", "Umbu", "Maça", "Tangerina" "Uva"]
+console.log(frutas);
+
+frutas.unshift("Banana"); // Adiciona ao inicio da lista ["Banana", "Cajá", "Umbu", "Maça", "Tangerina" "Uva"]
+console.log(frutas);
+
+frutas.pop(); // Remove o ultimo elemento da lista
+console.log(frutas);
+
+frutas.shift(); // Remove o primeiro elemento da lista
+console.log(frutas);
+
+// const usuarios = [
+//     {nome: "Alice", idade: 25, email: "alice@email.com"},
+//     {nome: "Ítalo", idade: 25, email: "alice@email.com"},
+//     {nome: "Mariane", idade: 25, email: "alice@email.com"},
+//     {nome: "Kauã", idade: 25, email: "alice@email.com"},
+//     {nome: "Isabel", idade: 25, email: "alice@email.com"}
+// ]
---
+++ variaveis.js
@@ -0,0 +1,38 @@
+// Variável declarada, indica que não possui valor atribuido
+var nome;
+
+// Variável inicialozada, possui valor atribuido
+const idade = 39;
+let naturalidade = "Salvador";
+
+let email = "prof.danilomiguel@ulife.com.br"; // String(texto)
+let altura = 1.8; //Number
+let aprovado = false; // Valores lógicos, boleanos True ou False
+
+// Conversão de Tipos de Dados
+
+// String para valores númericos
+let num = Number("42");
+let float = parseFloat("5,5");
+let inteiro = parseInt("10");
+
+// Número para String
+
+let str = String(100);
+let outraStr = (90).toString;
+
+// idade = 27
+// console.log(idade)
+
+// naturalidade = "São Paulo"
+// console.log(naturalidade)
+
+let numero1 = 10;
+let numero2 = 20;
+let soma = numero1 + numero2;
+console.log(soma);
+
+// Variável inicializada
+// let naturalidade = "São Paulo"
+// let idade  = 39
+// const PI  = 3.14
